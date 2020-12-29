c6330f3905eba87b473a6ec6062b274f4ed74361
akashdeep-19 <akasdeeps19@gmail.com>
2020-12-29 21:24:53 +0530

    Class requirements added

--- src/connectionGene.js
+++ src/connectionGene.js
@@ -1,1 +1,3 @@
 //ConnectionGene class
+//Add all attributes from paper
+//Make a copy function that returns a deep copy of the object
--- src/node.js
+++ src/node.js
@@ -1,1 +1,3 @@
 // Node class
+//Add all attributes from paper
+//Make a copy function that returns a deep copy of the object
