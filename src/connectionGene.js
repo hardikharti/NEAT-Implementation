6e42fdc6441533442456d21b7783afebd85e1e8f
tharun634 <tharun634@gmail.com>
2021-01-12 23:56:40 +0530

    added mutateWeight

--- src/connectionGene.js
+++ src/connectionGene.js
@@ -1,19 +1,40 @@
 //ConnectionGene class
 class connectionGene {
-    //Add all attributes from paper
-    constructor(from, to, w, inno,enbl) {
-        this.out = to;
-        this.in = from;
-        this.weight = w;
-        this.enabled = enbl;
-        this.innovationNo = inno;
-        
+  //Add all attributes from paper
+  constructor(from, to, w, inno, enbl) {
+    this.out = to;
+    this.in = from;
+    this.weight = w;
+    this.enabled = enbl;
+    this.innovationNo = inno;
+  }
+  //helper function to mutate
+  mutateCgWeight() {
+    var changeProb = Math.random(1);
 
+    if (changeProb < 0.1) {
+      this.weight = random(-1, 1);
+    } else {
+      this.weight += randomGaussian() / 25; //returns a mean of 0 and standard deviation of 0.04
+
+      if (this.weight > 1) {
+        this.weight = 1;
+      }
+      if (this.weight < -1) {
+        this.weight = -1;
+      }
     }
-    //Make a copy function that returns a deep copy of the object
-    deepCopy(from, to) {
-        var dCopy = new connectionGene(from, to, this.weight, this.innovationNo, this.enabled);
-        
-        return dCopy;
-    }
+  }
+  //Make a copy function that returns a deep copy of the object
+  deepCopy(from, to) {
+    var dCopy = new connectionGene(
+      from,
+      to,
+      this.weight,
+      this.innovationNo,
+      this.enabled
+    );
+
+    return dCopy;
+  }
 }
--- src/genome.js
+++ src/genome.js
@@ -1,111 +1,112 @@
-class Genome{
-    constructor(input_size,output_size,innv){
-        this.input_size = input_size;
-        this.output_size = output_size;
-        this.node_size = 0;
-        this.cg_size = 0;
-        this.layer_size = 0;
-        this.nodes = []; //INDEX -> node_id VALUE -> node 
-        this.biases = [];//INDEX -> layer VALUE -> bias
-        this.cgs = []; //VALUE -> connectionGene
-        this.node_adj = []; //INDEX -> node_id  VALUE -> Array of innovation no.s, the node is connected
-        this.layers = []; //INDEX -> layer VALUE -> Array of node_ids
-        this.cg_map = {}; //KEY -> innovation_no. VALUE -> connectionGene_index in this.cgs
-        this.innov = innv;
+class Genome {
+  constructor(input_size, output_size, innv) {
+    this.input_size = input_size;
+    this.output_size = output_size;
+    this.node_size = 0;
+    this.cg_size = 0;
+    this.layer_size = 0;
+    this.nodes = []; //INDEX -> node_id VALUE -> node
+    this.biases = []; //INDEX -> layer VALUE -> bias
+    this.cgs = []; //VALUE -> connectionGene
+    this.node_adj = []; //INDEX -> node_id  VALUE -> Array of innovation no.s, the node is connected
+    this.layers = []; //INDEX -> layer VALUE -> Array of node_ids
+    this.cg_map = {}; //KEY -> innovation_no. VALUE -> connectionGene_index in this.cgs
+    this.innov = innv;
+  }
+
+  // <=====HELPERS=====> //
+
+  addNode(node) {
+    this.nodes[node.id] = node;
+
+    this.node_adj[node.id] = [];
+
+    if (!this.layers[node.layer]) this.layers[node.layer] = [];
+
+    this.layers[node.layer].push(node.id);
+
+    this.node_size++;
+    this.layer_size = Math.max(this.layer_size, node.layer + 1);
+  }
+
+  addConnection(cg) {
+    let i;
+    for (i = this.cgs.length - 1; i >= 0; i--) {
+      if (this.cgs[i].innov_no > cg.innov_no) {
+        this.cgs[i + 1] = this.cgs[i];
+        this.cg_map[this.cgs[i + 1].innov_no] = i + 1;
+      } else break;
     }
+    this.cgs[i + 1] = cg;
 
-    // <=====HELPERS=====> //
+    this.cg_map[cg.innov_no] = i + 1;
 
-    addNode(node){
-        this.nodes[node.id] = node;
+    this.node_adj[cg.in.id].push(cg.innov_no);
+    this.node_adj[cg.out.id].push(cg.innov_no);
 
-        this.node_adj[node.id] = [];
+    this.cg_size++;
+  }
 
-        if(!this.layers[node.layer])
-            this.layers[node.layer] = [];
+  getCg(innov) {
+    if (!(innov in this.cg_map)) return false;
+    let i = this.cg_map[innov];
+    return this.cgs[i];
+  }
 
-        this.layers[node.layer].push(node.id);
-
-        this.node_size++;
-        this.layer_size = Math.max(this.layer_size,node.layer+1);
+  shiftLayer(layer) {
+    for (let i = this.layer_size - 1; i >= layer; i--) {
+      this.layers[i + 1] = [];
+      for (let id of this.layers[i]) {
+        this.nodes[id].layer++;
+        this.layers[i + 1].push(id);
+      }
+      if (i == this.layer_size - 1) continue;
+      this.biases[i + 1] = this.biases[i];
     }
+    this.layers[layer] = [];
+    this.biases[layer] = 2 * Math.random() - 1;
+    this.layer_size++;
+  }
 
-    
-    addConnection(cg){
-        let i;
-        for(i = this.cgs.length-1;i >= 0;i--){
-            if(this.cgs[i].innov_no > cg.innov_no){
-                this.cgs[i+1] = this.cgs[i];
-                this.cg_map[this.cgs[i+1].innov_no] = i+1;
-            }
-            else break;
-        }
-        this.cgs[i+1] = cg;
+  // <====INITIAL CONNECTIONS====> //
+  createInitialNodes() {
+    for (let i = 0; i < this.input_size; i++)
+      this.addNode(new Node(i, 0, "input"));
+    for (let i = this.input_size; i < this.input_size + this.output_size; i++)
+      this.addNode(new Node(i, 1, "output"));
 
-        this.cg_map[cg.innov_no] = i+1;
+    this.biases[0] = 2 * Math.random() - 1;
+    this.layer_size = 2;
+  }
 
-        this.node_adj[cg.in.id].push(cg.innov_no);
-        this.node_adj[cg.out.id].push(cg.innov_no);
-
-        this.cg_size++;
+  createDenseGenome() {
+    for (let i = 0; i < this.input_size; i++) {
+      for (
+        let j = this.input_size;
+        j < this.input_size + this.output_size;
+        j++
+      ) {
+        const cg = this.innov.createConnection(
+          this.nodes[i],
+          this.nodes[j],
+          2 * Math.random() - 1,
+          true
+        );
+        this.addConnection(cg);
+      }
     }
+  }
 
-    getCg(innov){
-        if(!(innov in this.cg_map))
-            return false;
-        let i = this.cg_map[innov];
-        return this.cgs[i];
+  // <====MUTATION====> //
+  mutate() {}
+
+  mutateWeight() {
+    for (var i = 0; i < this.cgs.length; i++) {
+      this.cgs[i].mutateCgWeight();
     }
+  }
 
-    shiftLayer(layer){
-        for(let i = this.layer_size-1;i >= layer;i--){
-            this.layers[i+1] = [];
-            for(let id of this.layers[i]){
-                this.nodes[id].layer++;
-                this.layers[i+1].push(id);
-            }
-            if(i == this.layer_size-1)continue;
-            this.biases[i+1] = this.biases[i];
-        }
-        this.layers[layer] = [];
-        this.biases[layer] = 2*Math.random()-1;
-        this.layer_size++;
-    }
+  mutateNode() {}
 
-    // <====INITIAL CONNECTIONS====> //
-    createInitialNodes(){
-        for(let i = 0;i < this.input_size;i++)
-            this.addNode(new Node(i,0,'input'));
-        for(let i = this.input_size;i < this.input_size + this.output_size;i++)
-            this.addNode(new Node(i,1,'output'));
-
-        this.biases[0] = 2*Math.random()-1;
-        this.layer_size = 2;
-    }
-
-    createDenseGenome(){
-        for(let i = 0;i < this.input_size;i++){
-            for(let j = this.input_size;j < this.input_size+this.output_size;j++){
-                const cg = this.innov.createConnection(this.nodes[i],this.nodes[j],2*Math.random()-1,true);
-                this.addConnection(cg);
-            }
-        }
-    }
-
-    // <====MUTATION====> //
-    mutate(){
-        
-    }
-
-    mutateWeight(){
-        
-    }
-
-    mutateNode(){
-        
-    }
-
-    mutateCg(){ 
-        
-    }
+  mutateCg() {}
 }
