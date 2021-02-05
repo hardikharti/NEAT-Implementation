f767e48de277e350bda27f9f953f69a91745f26b
tharun634 <tharun634@gmail.com>
2021-02-05 22:17:23 +0530

    implemented basic specie functions

--- src/innovationNum.js
+++ src/innovationNum.js
@@ -1,24 +1,23 @@
-class InnovNum{
-    constructor()
-    {
+class InnovNum {
+    constructor() {
         this.innovations = 0;
         this.innovations_lookup = {}; // KEY -> from_node_id VALUE -> {KEY->to_node_id VALUE->innovation_no}
     }
 
-    createConnection(from,to,weight,enabled){
-        if(this.getInnovNum(from,to) != -1)
-            return new connectionGene(from,to,weight,this.getInnovNum(from,to),enabled);
-            
-        if(!(from.id in this.innovations_lookup))
+    createConnection(from, to, weight, enabled) {
+        if (this.getInnovNum(from, to) != -1)
+            return new connectionGene(from, to, weight, this.getInnovNum(from, to), enabled);
+
+        if (!(from.id in this.innovations_lookup))
             this.innovations_lookup[from.id] = {};
         this.innovations_lookup[from.id][to.id] = this.innovations;
-        const cg = new connectionGene(from,to,weight,this.innovations,enabled);
+        const cg = new connectionGene(from, to, weight, this.innovations, enabled);
         this.innovations++;
         return cg;
     }
 
-    getInnovNum(from,to){
-        if(from.id in this.innovations_lookup && to.id in this.innovations_lookup[from.id])
+    getInnovNum(from, to) {
+        if (from.id in this.innovations_lookup && to.id in this.innovations_lookup[from.id])
             return this.innovations_lookup[from.id][to.id];
         return -1;
     }
--- src/node.js
+++ src/node.js
@@ -1,15 +1,15 @@
 // Node class
-class Node{
+class Node {
   //Add all attributes from paper
-  constructor(id,layer,type){
+  constructor(id, layer, type) {
     this.id = id;
-    this.type=type;
+    this.type = type;
     this.layer = layer;
     this.output = undefined;
   }
-//Make a copy function that returns a deep copy of the object
-  deepCopy(type){
-    var dCopy=new Node(this.id, this.layer,type);
+  //Make a copy function that returns a deep copy of the object
+  deepCopy(type) {
+    var dCopy = new Node(this.id, this.layer, type);
     return dCopy;
   }
 }
