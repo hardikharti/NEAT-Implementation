9f73fb5be8da8f3be6836db8c97ff7d5e40b56a7
akashdeep-19 <akasdeeps19@gmail.com>
2021-02-05 21:29:16 +0530

    added specie template

--- src/specie.js
+++ src/specie.js
@@ -1,11 +1,15 @@
 
 // class Specie
-    //Attributes
-        //rep
-        //members
-    //Methods - 
-        // addClient
-        // changeRep
-        // normalizeFitness
-        // killMembers
-        // nextGen
+//     Attributes
+//         rep = member
+//         members = []
+//     Methods - 
+//         addClient -> arg -> member if dist < threshold add to specie
+//         changeRep -> change rep to a different member in members
+//         normalizeFitness -> total fitness and member.fitness /= totalfitness
+//         killMembers -> n -> removes bottom n members
+//         nextGen -> n -> return array of members formed through crossover and mutation of existing members
+
+// class member
+//     fitness 
+//     genome
